325ee99d4fec8882a648037f56389d265b2be851
dzelin01 <daniellezelin@gmail.com>
2016-02-08 16:34:03 -0500

    Initial front end for incidents

---
+++ incidents/app/assets/javascripts/priority.js
@@ -0,0 +1,17 @@
+function highlight_priorities() {
+	grid = $("table")
+
+	grid.find('tr').each(function (i) {
+	   //$(this).css({ background: "#FFF" });//  <= this line
+	   // check to see if search term matches Name column
+		priority = $('td:first', $(this)).text();
+		if (priority >= 8) {
+			$(this).addClass("danger")
+		} else if (priority >= 5) {
+			$(this).addClass("warning")
+		}
+	   //if ($(this).find('td:first-child').text().toUpperCase().match(text.toUpperCase()))
+			   //$(this).css({ background: "#FC6" }); 
+			//  $(this).show(); // show matching row
+	});
+}
