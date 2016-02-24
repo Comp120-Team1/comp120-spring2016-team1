d2a8c12b095a58c46376322e8a7af9b148331bc7
Zach Kirsch <zachkirsch@gmail.com>
2016-02-24 15:21:57 -0500

    edit page formatted to look like new incident page

--- app/assets/javascripts/priority.js
+++ app/assets/javascripts/priority.js
@@ -1,31 +1,34 @@
 function highlight_priorities() {
-	grid = $("table")
+  $(document).ready( function() {
+    grid = $("table")
 
-	// grid.find('tr').each(function (i) {
-	//    //$(this).css({ background: "#FFF" });//  <= this line
-	//    // check to see if search term matches Name column
-	// 	priority = $('td:first', $(this)).text();
-	// 	console.log(priority)
-	// 	if (priority == "!!!") {
-	// 		$(this).addClass("danger");
-	// 	} else if (priority == "!!") {
-	// 		$(this).css("background-color", "#fff5e6");
-	// 	} else if (priority == "!") {
-	// 		$(this).addClass("warning");
-	// 	}
-	//    //if ($(this).find('td:first-child').text().toUpperCase().match(text.toUpperCase()))
-	// 		   //$(this).css({ background: "#FC6" });
-	// 		//  $(this).show(); // show matching row
-	// });
+      // grid.find('tr').each(function (i) {
+      //    //$(this).css({ background: "#FFF" });//  <= this line
+      //    // check to see if search term matches Name column
+      //  priority = $('td:first', $(this)).text();
+      //  console.log(priority)
+      //  if (priority == "!!!") {
+      //    $(this).addClass("danger");
+      //  } else if (priority == "!!") {
+      //    $(this).css("background-color", "#fff5e6");
+      //  } else if (priority == "!") {
+      //    $(this).addClass("warning");
+      //  }
+      //    //if ($(this).find('td:first-child').text().toUpperCase().match(text.toUpperCase()))
+      //       //$(this).css({ background: "#FC6" });
+      //    //  $(this).show(); // show matching row
+      // });
 
-	$('td:first-child').each(function() {
-		priority = $(this).text();
-		if (priority == "!!!") {
-	 		$(this).addClass("danger");
-	 	} else if (priority == "!!") {
-	 		$(this).css("background-color", "#fff5e6");
-	 	} else if (priority == "!") {
-	 		$(this).addClass("warning");
-	 	}
-	});
+      $('td:first-child').each(function() {
+        priority = $(this).text();
+        if (priority == "!!!") {
+          $(this).addClass("danger");
+        } else if (priority == "!!") {
+          $(this).css("background-color", "#fff5e6");
+        } else if (priority == "!") {
+          $(this).addClass("warning");
+        }
+      });
+  });
 }
+
