097a097a0d0b11a75b2b5adc56765a8b30b2a0ce
Michael Seltzer <michael.m.seltzer@gmail.com>
2016-03-02 13:42:07 -0500

    cloudfront cdn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # load data from cloudfront
+  config.serve_static_assets = true
+  config.static_cache_control = 'public, max-age=31536000'
+  config.action_controller.asset_host = ENV['AWS_CLOUDFRONT_URL']
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
