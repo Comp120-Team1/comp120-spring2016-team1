8d7d4de56c6b3a55200db4100ae387166c8f1e66
Michael Seltzer <michael.m.seltzer@gmail.com>
2016-03-09 14:52:06 -0500

    environmental path

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -9,6 +9,7 @@ Swagger::Docs::Config.register_apis({
   '1.0' => {
     controller_base_path: '',
     api_file_path: 'public/apidocs',
+    base_path: ENV['HOST_URL'] || 'http://localhost:3000',
     clean_directory: true
   }
 })
